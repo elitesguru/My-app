0ab38dc1b5c2987ef2f335f64649e5f22d99482e
john barack <100470663+elitesguru@users.noreply.github.com>
2024-05-08 19:01:54 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/smssender/MainActivity.java
@@ -0,0 +1,174 @@
+package com.example.smssender;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    Button btnSendSMS;
+    IntentFilter intentFilter;
+    EditText phoneNumber, messageSend;
+    TextView test;
+    private static final int RESULT_PICK_CONTACT =1;
+    private Button select;
+    String phonePick;
+
+    private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView SMSes = (TextView) findViewById(R.id.textView1);
+            SMSes.setText(intent.getExtras().getString("sms"));
+        }
+    };
+
+    @SuppressLint("MissingInflatedId")
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SMS_RECEIVED_ACTION");
+
+        btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
+        phoneNumber = (EditText) findViewById(R.id.phone_number);
+        messageSend = (EditText) findViewById(R.id.message_here);
+        test = (TextView) findViewById(R.id.textView1);
+        select = (Button) findViewById (R.id.select);
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+
+                sendSMS(phoneNumber.getText().toString(), messageSend.getText().toString());
+                messageSend.setText("");
+                phoneNumber.setText("");
+
+            }
+        });
+
+        select.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent (Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult (in, RESULT_PICK_CONTACT);
+            }
+        });
+    }
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+
+        try {
+            String phoneNo = null;
+            Uri uri = data.getData ();
+            cursor = getContentResolver ().query (uri, null, null,null,null);
+            cursor.moveToFirst ();
+            int phoneIndex = cursor.getColumnIndex (ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            phoneNo = cursor.getString (phoneIndex);
+
+            phoneNumber.setText (phoneNo);
+
+
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        if(resultCode==RESULT_OK)
+        {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked (data);
+                    break;
+            }
+        }
+        else
+        {
+            Toast.makeText (this, "Failed To pick contact", Toast.LENGTH_SHORT).show ();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(intentReceiver, intentFilter);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(intentReceiver);
+        super.onPause();
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                }
+            }, new IntentFilter(SENT));
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                }
+            }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+    }
+}
